c05f62a2c57236075740d8c97bca9a377b60ad0e
Kirill <c.derche@me.com>
2016-05-23 15:26:34 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,27 @@
+var host  = null
+var aKey  = null
+var pKey  = null
+
+module.exports = function(paytureHost, addKey, payKey) {
+
+  host  = paytureHost
+  aKey  = addKey
+  pKey  = payKey
+
+  checkArgs()
+
+  function checkArgs() {
+    if (typeof host != 'string') {
+      throw console.error("Host must be a String");
+    }
+    if (typeof aKey != 'string') {
+      throw console.error("aKey must be a String");
+    }
+    if (typeof pKey != 'string') {
+      throw console.error("pKey must be a String");
+    }
+  }
+
+  console.log("Welcome to the Payture API for NodeJS");
+
+}
